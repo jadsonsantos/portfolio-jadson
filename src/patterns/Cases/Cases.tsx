d9556f7279e076ea1a1718ebf172718831e58102
Jadson Santos <jadsondossantos5@gmail.com>
2023-11-25 19:00:41 -0300

    chore: remove lint staged

--- src/patterns/Cases/Cases.tsx
+++ src/patterns/Cases/Cases.tsx
@@ -1,11 +1,11 @@
-import Case from 'components/Case'
+// import Case from 'components/Case'
 import Container from 'components/Container'
 import Heading from 'components/foundation/Heading'
 import Typography from 'components/foundation/Typography'
 
 import styles from './Cases.module.scss'
 
-import cases from './data'
+// import cases from './data'
 
 const Cases = () => {
   return (
@@ -14,7 +14,7 @@ const Cases = () => {
         <Heading>Projetos</Heading>
         <Typography>Conheça alguns dos projetos que desenvolvi</Typography>
         <section className={styles.cases__list}>
-          {cases.map(({ title, image, description, tags, link }) => (
+          {/* {cases.map(({ title, image, description, tags, link }) => (
             <Case
               key={title}
               title={title}
@@ -24,7 +24,7 @@ const Cases = () => {
               tags={tags}
               link={link}
             />
-          ))}
+          ))} */}
         </section>
       </Container>
     </section>
