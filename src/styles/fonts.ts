cbd4aaadbdfe53832089d6eaaa84d2e2914feb6a
Jadson Santos <jadsondossantos5@gmail.com>
2023-12-27 12:59:25 -0300

    feat: add new font

---
+++ src/styles/fonts.ts
@@ -0,0 +1,17 @@
+import { Roboto, Inter } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  variable: '--font-primary'
+})
+
+const inter = Inter({
+  weight: ['700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  variable: '--font-secondary'
+})
+
+export { roboto, inter }
