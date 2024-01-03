4eb6864c3b60c8085014f7c139545c00722c50a2
Jadson Santos <jadsondossantos5@gmail.com>
2024-01-03 16:10:29 -0300

    feat: add cv to about section

--- src/components/foundation/Typography/index.tsx
+++ src/components/foundation/Typography/index.tsx
@@ -1,5 +1,5 @@
 type Props = {
-  children: string
+  children: React.ReactNode
   className?: React.ReactNode
 }
 
--- src/patterns/About/About.tsx
+++ src/patterns/About/About.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import Container from 'components/Container'
+import CustomLink from 'components/CustomLink'
 import Heading from 'components/foundation/Heading'
-import Typograph from 'components/foundation/Typography'
+import Typography from 'components/foundation/Typography'
 import Skill from 'components/Skill'
 
 import styles from './About.module.scss'
@@ -17,22 +18,20 @@ const About = () => {
       <Container className={styles.about__container}>
         <div className={styles.about__infos}>
           <Heading>Quem sou eu</Heading>
-          <Typograph>{data.about}</Typograph>
+          <Typography className={styles.about__text}>{data.about}</Typography>
+          <Typography>
+            <Link href="/cv.pdf">Acesse meu currículo</Link>
+          </Typography>
           <div className={styles.about__social}>
             {data.socialLinks.map(({ link, title }) => (
-              <Link
-                href={link}
-                target="_blank"
-                rel="noopener noreferer"
-                key={title}
-              >
+              <CustomLink href={link} target="_blank" key={title}>
                 <Image
                   src={`images/${title}.svg`}
                   width={28}
                   height={28}
                   alt={`Ícone do ${title}`}
                 />
-              </Link>
+              </CustomLink>
             ))}
           </div>
         </div>
