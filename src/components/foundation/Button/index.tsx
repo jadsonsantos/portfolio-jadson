337ab378b906e50c27baac4f64adef6471fa8bfc
Jadson Santos <jadsondossantos5@gmail.com>
2024-01-09 11:19:15 -0300

    feat: add button property to button component

--- src/components/foundation/Button/index.tsx
+++ src/components/foundation/Button/index.tsx
@@ -1,11 +1,17 @@
+import { ButtonHTMLAttributes } from 'react'
+
 import styles from './Button.module.scss'
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
 }
 
-const Button = ({ children }: ButtonProps) => {
-  return <button className={styles.button}>{children}</button>
+const Button = ({ children, ...rest }: ButtonProps) => {
+  return (
+    <button className={styles.button} {...rest}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
