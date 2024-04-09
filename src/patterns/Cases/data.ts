0d15ea1d57024d0c7673650574bf42021a1538c9
Jadson Santos <jadsondossantos5@gmail.com>
2024-04-09 02:17:58 -0300

    feat: add new case

--- src/patterns/Cases/data.ts
+++ src/patterns/Cases/data.ts
@@ -2,10 +2,10 @@ const cases = [
   {
     image: 'case-thalita.png',
     tags: 'html5 . css3 . sass . nextjs . emailjs',
-    title: 'Portfólio da Thalita Torres',
+    title: 'Portfólio de Thalita Torres',
     link: { site: 'https://thalitatorres.com.br/' },
     description:
-      'Este projeto visava destacar as habilidades, projetos e experiências da talentosa designer de produto Thalita Torres.'
+      'Este projeto foi criado para destacar as habilidades, projetos e experiências da talentosa designer de produto Thalita Torres, oferecendo uma visão abrangente de sua carreira e contribuições no campo do design de produto.'
   },
   {
     image: 'case-mercadolivre.png',
@@ -16,18 +16,29 @@ const cases = [
       github: 'https://github.com/jadsonsantos/shopping-cart'
     },
     description:
-      'Carrinho de compras com recursos como listagem de produtos, busca de produtos via API do Mercado Livre, adição e remoção de produtos do carrinho.'
+      'Aplicação que oferece uma experiência de compra personalizada, integrando-se à API do Mercado Livre para listar produtos, facilitando a busca e adição de itens ao carrinho de compras.'
   },
   {
     image: 'case-primeflix.png',
     tags: 'html5 . css3 . reactjs . typescript',
-    title: 'Filmaria com ReactJS',
+    title: 'Aplicativo de Filmes',
     link: {
       site: 'https://primeflix-psi.vercel.app/',
       github: 'https://github.com/jadsonsantos/prime-flix'
     },
     description:
-      'Projeto de filmes desenvolvido com ReactJS. Permite listar filmes, consumindo a API do The Movie DB, adicionar e remover filmes da lista de favoritos.'
+      'Plataforma interativa desenvolvida em ReactJS, que integra-se à API do The Movie DB para listar filmes, permitindo aos usuários adicionar e remover títulos da lista de favoritos com facilidade.'
+  },
+  {
+    image: 'case-clubecommerce.png',
+    tags: 'styled components . reactjs . typescript . stripe . firebase',
+    title: 'E-commerce ReactJS',
+    link: {
+      site: 'https://clubecommerce.jadsonsantos.dev.br/',
+      github: 'https://github.com/jadsonsantos/club-ecommerce-frontend'
+    },
+    description:
+      'Plataforma de e-commerce construída com ReactJS. Oferece listagem de produtos, gerenciamento de carrinho de compras, checkout seguro com Stripe, e autenticação via email/senha ou Google. Projetada para uma experiência de usuário fluida e segura.'
   }
 ]
 
