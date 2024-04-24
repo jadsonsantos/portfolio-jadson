be1d59d3a1e9dc4c728154a88ab8cef99f19e500
Jadson Santos <jadsondossantos5@gmail.com>
2024-04-24 15:31:08 -0300

    feat: add mobile util and refactor title component

--- src/components/foundation/Heading/index.tsx
+++ src/components/foundation/Heading/index.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 type HeadingProps = React.ComponentProps<'h2'> & {
   className?: string
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 }
 
-const Heading = ({ children, className }: HeadingProps) => {
-  return <h2 className={`heading ${className || ''}`}>{children}</h2>
+const Heading = ({ children, className, as = 'h2' }: HeadingProps) => {
+  const customClassName = className ? `heading ${className}` : 'heading'
+  const Element = as
+
+  return <Element className={customClassName}>{children}</Element>
 }
 
 export default Heading
