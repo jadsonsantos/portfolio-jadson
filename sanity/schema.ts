145ed7dc57f805966b9f04b6fd9d14d0b167b034
Jadson Santos <jadsondossantos5@gmail.com>
2024-05-19 16:20:14 -0300

    feat: add profile schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,12 @@
 import author from './schemaTypes/author'
 import blockContent from './schemaTypes/blockContent'
+import _case from './schemaTypes/case'
 import category from './schemaTypes/category'
 import post from './schemaTypes/post'
+import profile from './schemaTypes/profile'
 
 import { type SchemaTypeDefinition } from 'sanity'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent]
+  types: [post, author, category, blockContent, _case, profile]
 }
--- src/patterns/Hero/Hero.tsx
+++ src/patterns/Hero/Hero.tsx
@@ -7,28 +7,36 @@ import Typography from 'components/foundation/Typography'
 
 import styles from './Hero.module.scss'
 
+import { ProfileType } from 'types'
+
 import profilePicture from '../../../public/images/about.svg'
 import heroArrow from '../../../public/images/hero-arrow.svg'
 import heroIllustration from '../../../public/images/hero-illustration-1.svg'
+import { getProfile } from '../../../sanity/lib/sanity.query'
 
-const Hero = () => {
+const Hero = async () => {
+  const profile = await getProfile()
+
   return (
     <section className={styles.hero}>
       <Container className={styles.hero__container}>
         <div className={styles.hero__wrapper}>
-          <div className={styles.hero__infos}>
-            <Image
-              src={heroIllustration}
-              alt="Ilustração de uma barra"
-              priority
-            />
-            <h4 className={styles.hero__presentation}>Olá, eu sou o</h4>
-            <Heading>Jadson Santos</Heading>
-            <Typography>
-              Desenvolvedor Front-End apaixonado por criar experiências web
-              excepcionais que sejam rápidas e responsivas.
-            </Typography>
-          </div>
+          {profile &&
+            profile.map((data: ProfileType) => (
+              <div className={styles.hero__infos} key={data._id}>
+                <Image
+                  src={heroIllustration}
+                  alt="Ilustração de uma barra"
+                  priority
+                />
+                <h4 className={styles.hero__presentation}>Olá, eu sou o</h4>
+                <Heading>{data.fullName}</Heading>
+                <Typography>
+                  Desenvolvedor Front-End apaixonado por criar experiências web
+                  excepcionais que sejam rápidas e responsivas.
+                </Typography>
+              </div>
+            ))}
           <Image src={profilePicture} alt="Foto do Jadson" priority />
         </div>
         <Link className={styles.hero__arrow} href="#about">
