b8072f29ebd2838b96fb731cb636ba4ff66d05b7
Jadson Santos <jadsondossantos5@gmail.com>
2024-05-19 16:21:33 -0300

    chore: some adjustments

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,9 +2,15 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
-export const client = createClient({
+import { ClientConfig } from '@sanity/client'
+
+const config: ClientConfig = {
   apiVersion,
   dataset,
   projectId,
   useCdn
-})
+}
+
+const client = createClient(config)
+
+export default client
